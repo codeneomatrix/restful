81c41b81bbd4a61e0c2b9fab328d64b70635c056
neomatrix <josuecevedo@gmail.com>
2015-12-04 00:30:15 -0600

    cargando los datos

--- ser.js
+++ ser.js
@@ -1,14 +1,19 @@
 var restify = require('restify');
 
 function respond(req, res, next) {
-  res.send('HOLA:  ' + req.params.name);
+  var cuerpo_del_mensaje = {
+    "mensaje": 'HOLA:' + req.params.name
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.send(cuerpo_del_mensaje);
   next();
 }
 
 var server = restify.createServer();
 server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
+
